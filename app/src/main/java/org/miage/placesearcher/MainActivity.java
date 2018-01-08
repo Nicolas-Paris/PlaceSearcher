9b2887d40807da507e6ac2900cb4793a8e38249f
Nicolas-Paris <nicoparis.44@hotmail.fr>
2018-01-08 15:55:53 +0100

    Initilisation

---
+++ app/src/main/java/org/miage/placesearcher/MainActivity.java
@@ -0,0 +1,38 @@
+package org.miage.placesearcher;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.hello)  TextView hello;
+    @BindView(R.id.ratingBar) RatingBar ratingBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        ratingBar.setProgress(ratingBar.getProgress() + 1);
+    }
+
+    @OnClick(R.id.hello)
+    public void clickedOnTextField() {
+            ratingBar.setProgress(ratingBar.getProgress() - 1);
+    }
+
+}
