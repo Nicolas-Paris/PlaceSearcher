f803d68aca31b1401b6d010c49754ed809c3ae80
Nicolas-Paris <nicoparis.44@hotmail.fr>
2018-01-08 15:58:23 +0100

    Ajout commentaire test

--- app/src/main/java/org/miage/placesearcher/MainActivity.java
+++ app/src/main/java/org/miage/placesearcher/MainActivity.java
@@ -19,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Utilisation de la librairie ButterKnife
         ButterKnife.bind(this);
 
     }
