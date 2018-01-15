57500221c3d38ec03550e4e69630e0f6f612f942
Nicolas-Paris <nicoparis.44@hotmail.fr>
2018-01-15 16:32:24 +0100

    ajout appel REST

--- app/src/main/java/org/miage/placesearcher/MainActivity.java
+++ app/src/main/java/org/miage/placesearcher/MainActivity.java
@@ -3,8 +3,10 @@ package org.miage.placesearcher;
 import android.content.Intent;
 import android.content.res.AssetFileDescriptor;
 import android.media.MediaPlayer;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -16,6 +18,9 @@ import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -66,6 +71,32 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+
+        AsyncTask<String, Void, Response> asyncTask = new AsyncTask<String, Void, Response>() {
+            @Override
+            protected Response doInBackground(String... strings) {
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request request = new Request.Builder().url(strings[0])
+                        .build();
+                try {
+                    Response response = okHttpClient.newCall(request).execute();
+                    return response;
+                } catch(IOException e) {
+                    Log.e("ERROR BITCH", e.getMessage());
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Response response) {
+                super.onPostExecute(response);
+
+                Log.d("Test", response.body().toString());
+            }
+        };
+
+        asyncTask.execute("http://api.adresse.data.gouv.fr/search/?q=Place%20du%20commerce");
+
 
     }
 
